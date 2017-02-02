c59772ef9e69b36603c8ecead10afd946cda0bc4
hirekaanmicheal@gmail.com <hirekaanmicheal@gmail.com>
2017-02-02 16:36:57 +0100

    inital commit with orm

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/api.js
+++ routes/api.js
@@ -11,4 +11,24 @@ api.get('/json', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
+api
+	.route('/api/account/types')
+		.get((req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.status(200).jsonp({ text: 'get sent' });
+		})
+		.post((req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.status(200).json({ text: 'post sent' });
+		})
+		.put((req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.status(200).json({ text: 'put sent' });
+		})
+		.delete((req, res) => {
+			res.setHeader('Content-Type', 'application/json');
+			res.status(200).json({ text: 'delete sent' });
+		})
+
+
 module.exports = api;
